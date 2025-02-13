07f6e453d6dfb1afa231ffd72b97c6d63b9bae9e
Elcand <tjandraraharja99@gmail.com>
2025-02-13 11:51:09 +0700

    Feat modif notif

--- app/Filament/Resources/BarangResource/Pages/CreateBarang.php
+++ app/Filament/Resources/BarangResource/Pages/CreateBarang.php
@@ -4,9 +4,21 @@ namespace App\Filament\Resources\BarangResource\Pages;
 
 use App\Filament\Resources\BarangResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateBarang extends CreateRecord
 {
     protected static string $resource = BarangResource::class;
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Sukses')
+            ->icon('heroicon-o-document-check')
+            ->color('success')
+            ->duration(5000)
+            ->body('Data Barang Berhasil Ditambahkan!');
+    }
 }
